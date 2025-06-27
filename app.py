4187bf75bbde4fad4763e3f0a49309adc668b0d0
Paulo Lima <phl.lhp@hotmail.com>
2025-06-27 19:22:17 -0300

    Primeiro commit do projeto GlowFit com estrutura completa

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash
+import yagmail
 
 app = Flask(__name__)
+app.secret_key = 'segredo_super_secreto'
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/sobre')
-def sobre():
-    return render_template('sobre.html')
-
-@app.route('/depoimentos')
-def depoimentos():
-    return render_template('depoimentos.html')
-
-@app.route('/contato')
+@app.route('/contato', methods=['GET', 'POST'])
 def contato():
-    return render_template('contato.html')
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        mensagem = request.form['mensagem']
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        # Envia e-mail
+        try:
+            yag = yagmail.SMTP('SEUEMAIL@gmail.com', 'SENHA-DO-APLICATIVO')
+            yag.send(
+                to='SEUEMAIL@gmail.com',
+                subject='Nova mensagem do formulário de contato',
+                contents=f'Nome: {nome}\nEmail: {email}\nMensagem:\n{mensagem}'
+            )
+            flash('Mensagem enviada com sucesso!')
+        except Exception as e:
+            flash(f'Erro ao enviar: {e}')
+
+        return redirect('/contato')
+    return render_template('contato.html')
