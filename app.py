58a988c626cbe9d3484406a6065662d65d2eb445
Paulo Lima <phl.lhp@hotmail.com>
2025-06-27 18:47:31 -0300

    Primeiro commit do produto GlowFit

---
+++ app.py
@@ -0,0 +1,23 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+@app.route('/depoimentos')
+def depoimentos():
+    return render_template('depoimentos.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
